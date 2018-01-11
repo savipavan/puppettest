ec1cacd71539a5e83806f203dcd768f8c3ad8eff
savipavan <savipavan@gmail.com>
2018-01-11 16:03:06 +0530

    Add files via upload

---
+++ Check_mk agent install.pp
@@ -0,0 +1,38 @@
+class check_mk::agent::install (
+  $version,
+  $filestore,
+  $workspace,
+) {
+  if ! defined(Package['xinetd']) {
+    package { 'xinetd':
+      ensure => present,
+    }
+  }
+  if $filestore {
+    if ! defined(File[$workspace]) {
+      file { $workspace:
+        ensure => directory,
+      }
+    }
+    file { "${workspace}/check_mk-agent-${version}.noarch.rpm":
+      ensure  => present,
+      source  => "${filestore}/check_mk-agent-${version}.noarch.rpm",
+      require => Package['xinetd'],
+    }
+    package { 'check_mk-agent':
+      ensure   => present,
+      provider => 'rpm',
+      source   => "${workspace}/check_mk-agent-${version}.noarch.rpm",
+      require  => File["${workspace}/check_mk-agent-${version}.noarch.rpm"],
+    }
+    
+      ],
+    }
+  }
+  else {
+    package { 'check_mk-agent':
+      ensure  => present,
+      require => Package['xinetd'],
+    }
+    }
+}
